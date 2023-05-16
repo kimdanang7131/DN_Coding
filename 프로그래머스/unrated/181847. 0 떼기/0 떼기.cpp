1404c0f519842ea740de416e468709b77876c384
kimdanang7131 <91853084+kimdanang7131@users.noreply.github.com>
2023-05-16 23:43:17 +0900

    [unrated] Title: 0 떼기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string n_str) {
+    string answer = "";
+    int temp = stoi(n_str);
+    answer = to_string(temp);
+    return answer;
+}
