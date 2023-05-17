792c429b0a98ca4e6a12b526bcbde8ca80dd5d87
kimdanang7131 <91853084+kimdanang7131@users.noreply.github.com>
2023-05-17 20:40:44 +0900

    [unrated] Title: ad 제거하기, Time: 0.23 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181870. ad 제거하기/ad 제거하기.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> solution(vector<string> strArr) {
+    vector<string> answer;
+    
+    for(auto s : strArr)
+    {
+        if(s.find("ad") == string::npos)
+        {
+            answer.push_back(s);
+        }
+    }
+    return answer;
+}
