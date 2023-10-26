68be6787cb03b52c1cd18843235867019f7e8fa2
kimdanang7131 <91853084+kimdanang7131@users.noreply.github.com>
2023-10-27 02:18:21 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int n) {
+    string answer = "";
+    
+    for(int i=1; i <= n; i++)
+        (i & 1) ? answer += "수" : answer +="박";
+    return answer;
+}
