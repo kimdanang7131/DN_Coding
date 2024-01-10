ef8f905b1bf61d4dbf580fac11e1cb6d85222e93
kimdanang7131 <91853084+kimdanang7131@users.noreply.github.com>
2024-01-11 05:14:16 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.cpp
+++ 프로그래머스/2/42842. 카펫/카펫.cpp
@@ -1,67 +1,21 @@
 #include <string>
 #include <vector>
-#include <iostream>
 
 using namespace std;
 
-vector<int> FindDivisors(const int& num)
-{
-    vector<int> divisors;
-    
-    if(!num)
-        return divisors;   
-        
-    if(num == 1)
-    {
-        divisors.push_back(1);
-        return divisors;
-    }
-    
-    divisors.push_back(1);
-   
-    
-    for(int i=2; i <= num / 2; i++)
-    {
-        if(num % i)
-            continue;
-        
-        divisors.push_back(i);
-    }
-     divisors.push_back(num);
-    
-    return divisors;
-}
+vector<int> solution(int brown, int red) {
 
-void Swap(int& a , int& b)
-{
-    int temp = a;
-    a = b;
-    b = temp;
-}
+    int len = brown / 2 + 2;
 
-vector<int> solution(int brown, int yellow) {
-    vector<int> answer;
-    vector<int> divisors(yellow,0);
-    
-    divisors = FindDivisors(yellow);
-    
-    for(int i : divisors)
-    {       
-        int temp = 0;
-        temp = (i * 2) + (yellow / i) * 2 + 4;
-        
-        int w = i;
-        int v = yellow / i;
-        
-        if(w < v)
-            continue;
-                
-        if(temp == brown)
-        {
-            answer.push_back(w + 2);
-            answer.push_back(v + 2);
-            break;
-        }
+    int w = len - 3;
+    int h = 3;
+
+    while(w >= h){
+        if(w * h == (brown + red)) break;
+
+        w--;
+        h++;
     }
-    return answer;
+
+    return vector<int>{w, h};
 }
