64108eeab856255140ce914d8ee86e958745380a
kimdanang7131 <91853084+kimdanang7131@users.noreply.github.com>
2024-01-29 23:33:47 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.03 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.cpp
@@ -0,0 +1,50 @@
+#include <string>
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
+vector<int> solution(int n, vector<string> words) {
+    vector<int> answer;
+    int number  = 1;
+    int count   = 1;
+    unordered_set<string> dedupeSet;
+    dedupeSet.insert(words[0]);
+    
+    bool check = false;
+    for(int i = 1; i < words.size(); i++)
+    {
+        number = (i % n) + 1;
+        count  = i / n + 1;
+        
+        int prevLastChar = words[i - 1].back();
+        
+        if(prevLastChar != words[i].front())
+        {
+            check = true;
+            break;
+        }
+        
+        if(dedupeSet.find(words[i]) != dedupeSet.end())
+        {
+            check = true;
+            break;
+        }
+        
+        dedupeSet.insert(words[i]);
+    }   
+    
+    if(check)
+    {
+        answer.push_back(number);
+        answer.push_back(count);
+    }
+    else
+    {
+        answer.push_back(0);
+        answer.push_back(0);
+    }
+   
+
+    return answer;
+}
