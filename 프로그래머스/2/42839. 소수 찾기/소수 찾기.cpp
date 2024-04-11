a5edb0959d313c39b456672b59638169b4eb8cc4
kimdanang7131 <91853084+kimdanang7131@users.noreply.github.com>
2024-04-12 08:18:46 +0900

    [level 2] Title: 소수 찾기, Time: 219.70 ms, Memory: 35 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
@@ -0,0 +1,75 @@
+#include <string>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+set<int> primeSet;
+set<int> answerSet;
+
+vector <bool> check; 
+vector <char> v;
+
+void EratosthenesInit(const int& n)
+{
+    vector<bool> primeVec(n + 1 , true);
+    primeVec[0] = false;
+    primeVec[1] = false;
+    
+    for(int i = 2; i * i <= n; i++)
+    {
+        if(primeVec[i])
+        {
+            for(int j = i * i; j <= n; j += i)
+                primeVec[j] = false;
+        }
+    }
+    
+    for(int i = 2; i <= n; i++)
+    {
+        if(primeVec[i])
+            primeSet.insert(i);
+    }
+}
+
+bool CheckPrimeNum(const int& primeNum)
+{
+    return primeSet.find(primeNum) != primeSet.end();
+}
+
+void dfs(int level, string s)
+{   
+    if(level >= 1)
+    {
+        if(CheckPrimeNum(stoi(s))) 
+            answerSet.insert(stoi(s)); 
+    }
+    
+    for(int i=0; i<v.size(); i++)
+    {
+        // 방문되었다면 넘어가기
+        if(check[i])
+            continue; 
+        
+        // 방문처리
+        check[i]=true; 
+        dfs(level+1, s+v[i]); 
+        
+        // 백트래킹
+        check[i]=false; 
+    }   
+}
+
+int solution(string numbers) {
+    EratosthenesInit(10000000);
+    
+    // char 벡터로 변환하여 저장
+    for(int i=0; i < numbers.size(); i++) 
+        v.push_back(numbers[i]);       
+    
+    // char 벡터의 크기로 방문을 기록하기 위한 bool vector
+    check.resize(v.size(),false); 
+    dfs(0, "");    
+    
+    return answerSet.size();
+}
