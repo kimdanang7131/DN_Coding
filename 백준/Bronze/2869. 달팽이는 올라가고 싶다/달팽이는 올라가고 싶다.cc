819e783ac266148b55a6bee03388c28560b4011d
kimdanang7131 <91853084+kimdanang7131@users.noreply.github.com>
2024-04-21 23:01:43 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+	int a, b, v;
+	int day = 1;
+	cin >> a >> b>> v;
+	
+	day += ( v - a) / (a - b);
+	
+	if((v -a) % (a - b)) cout << day + 1;
+	else cout << day;
+	
+    return 0;
+}
