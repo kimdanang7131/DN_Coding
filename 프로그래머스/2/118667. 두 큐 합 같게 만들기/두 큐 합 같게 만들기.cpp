7b3388fe2efb47cb07a977340d2596daea3fa9a1
kimdanang7131 <91853084+kimdanang7131@users.noreply.github.com>
2024-04-30 23:58:24 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 2.79 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.cpp
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.cpp
@@ -1,68 +1,48 @@
 #include <string>
+#include <iostream>
 #include <vector>
 #include <queue>
 
 using namespace std;
 
+long long solution(vector<int> queue1, vector<int> queue2) {
+    long long answer = 0;
+    queue<int> temp1;
+    queue<int> temp2;
+    long long sum=0, q1_sum=0, q2_sum=0;
+    long long max = -1;
 
-int solution(vector<int> queue1, vector<int> queue2) {
-    long long target = 0;
-    long long sumA = 0;
-    long long sumB = 0;
-    
-    queue<int> q1;
-    queue<int> q2;
-    for(int i=0; i<queue1.size(); i++)
-    {
-        sumA += queue1[i];
-        q1.push(queue1[i]);
+    for(int i=0; i<queue1.size();i++){
+        sum = sum+ queue1[i] + queue2[i];
+        temp1.push(queue1[i]);
+        temp2.push(queue2[i]);
+        q1_sum += queue1[i];
+        q2_sum += queue2[i];
+        if(queue1[i] > max) max = queue1[i];
+        if(queue2[i] > max) max = queue2[i];
     }
-    for(int i=0; i<queue2.size(); i++)
-    {
-        sumB += queue2[i];
-        q2.push(queue2[i]);
+
+
+    if(max > (sum/2)) return -1;
+
+    while(!temp1.empty() | !temp2.empty()){
+        if(q1_sum == q2_sum) return answer;
+        if(answer > queue1.size()*5) return -1;
+        if(q1_sum > q2_sum){
+            int numf = temp1.front();
+            temp2.push(numf);
+            q2_sum += numf;
+            q1_sum -= numf;
+            temp1.pop();
+       }
+        else{
+            long numf = temp2.front();
+            temp1.push(numf);
+            q1_sum += numf;
+            q2_sum -= numf;
+            temp2.pop(); 
+        }
+        answer++;   
     }
-    
-    target = sumA + sumB;
-    target /= 2;
-    
-    int answer = 0;
-    int maxCount = queue1.size() * 10;
-    while(!q1.empty() && !q2.empty())
-    {
-        if(answer >= maxCount)
-            return -1;
-        
-        if(sumA == target && sumB == target)
-        {
-            return answer;                
-        }
-        
-        int input = 0;
-        answer++;
-        maxCount++;
-        
-        if(sumA > sumB)
-        {
-            input = q1.front();
-            
-            q1.pop();
-            q2.push(input);
-            
-            sumA -= input;
-            sumB += input;
-        }
-        else if( sumA < sumB )
-        {
-            input = q2.front();
-            
-            q2.pop();
-            q1.push(input);
-            
-            sumA += input;
-            sumB -= input;
-        }
-    }
-    
     return -1;
 }
