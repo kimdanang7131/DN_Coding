989b8cc6db79824fca5de20daa7fcf878be04060
kimdanang7131 <91853084+kimdanang7131@users.noreply.github.com>
2024-05-14 22:11:15 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+
+
+int main()
+{	
+	cin >> n;
+		
+	if(n % 4 == 0 && ( n % 100 != 0 || n % 400 == 0 ))
+	{
+		cout << "1" << endl;
+	}
+	else
+		cout << "0" << endl;
+	
+	
+	return 0;
+}
