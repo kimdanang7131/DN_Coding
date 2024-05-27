b6a46ca59118d1266c3b18a93ba7870cd6e8e253
kimdanang7131 <91853084+kimdanang7131@users.noreply.github.com>
2024-05-28 02:05:39 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+string s;
+
+int main()
+{	
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	
+	cin >> s;
+	
+	cout << s.length() << endl;	
+	return 0;
+}
