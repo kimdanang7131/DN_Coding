7a407fce405ab09f8bbbaa7a7f9a32906bc4fd10
kimdanang7131 <91853084+kimdanang7131@users.noreply.github.com>
2024-05-28 19:41:48 +0900

    [level 2] Title: 124 나라의 숫자, Time: 0.01 ms, Memory: 3.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int n) {
+    string answer = ""; 
+    
+    while(n > 0)
+    {
+        int remain = n % 3;
+        n /= 3;
+        if( remain == 0)
+        {
+            answer = "4" + answer;
+            n-=1;
+        }
+        else
+            answer = to_string(remain) + answer;
+    }
+ 
+   
+    return answer;
+}
