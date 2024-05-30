0916b3a54a10473a1a8781a14592d35d78cbe54f
kimdanang7131 <91853084+kimdanang7131@users.noreply.github.com>
2024-05-30 20:08:43 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+//int alpha[26];
+
+int main()
+{	
+	string s;
+	while(getline(cin , s))
+	{
+		cout << s << endl;
+	}
+	
+	return 0;
+}
