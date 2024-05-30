941c8aac532f1308eb81756b2f82c2a0dd16426b
kimdanang7131 <91853084+kimdanang7131@users.noreply.github.com>
2024-05-30 20:15:32 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+//int alpha[26];
+
+int main()
+{	
+	printf("         ,r'\"7\n");
+	printf("r`-_   ,'  ,/\n");
+	printf(" \\. \". L_r'\n");
+	printf("   `~\\/\n");
+	printf("      |\n");
+	printf("      |\n");
+	return 0;
+}
