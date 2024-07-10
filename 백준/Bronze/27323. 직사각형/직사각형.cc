efaa82c9fef3e06613ccf61d2bbf6199ab55baba
kimdanang7131 <91853084+kimdanang7131@users.noreply.github.com>
2024-07-10 22:00:31 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int a , b;
+
+int main()
+{
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	
+	cin >> a >> b;
+	
+	cout << a * b  << endl;
+
+
+	return 0;
+}
