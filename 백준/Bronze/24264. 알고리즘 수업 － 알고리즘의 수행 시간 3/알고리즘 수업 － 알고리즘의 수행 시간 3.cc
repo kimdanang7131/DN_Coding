15368c72565e2882f788d31723c9c27ec65b0ddc
kimdanang7131 <91853084+kimdanang7131@users.noreply.github.com>
2024-07-18 18:05:23 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+long long n;
+
+int main()
+{
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	
+	cin >> n;
+	
+	cout << n * n << endl << 2 << endl;
+	
+
+	return 0;
+}
