c779bf31e39c2fd977f58c89f221bce1b711b73e
kimdanang7131 <91853084+kimdanang7131@users.noreply.github.com>
2024-08-14 18:30:57 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 40 ms, Memory: 3204 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
@@ -6,25 +6,38 @@ using namespace std;
 
 typedef pair<int,int> Point;
 
+bool compare(const Point& p1, const Point& p2)
+{
+	if(p1.first == p2.first)
+	{
+		return p1.second < p2.second;
+	}
+	else
+		return p1.first < p2.first;
+}
+
 int n;
 
 int main()
 { 
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
 	cin >> n;
 	
 	vector<Point> pointVec(n);
 	
-	int a,b;
-	
 	for(int i=0; i<n; i++)
 	{
-		
+		int a,b;
 		cin >> a >> b;
 		
 		pointVec[i] = {a,b};
 	}
+
 	
-	sort(pointVec.begin() , pointVec.end());
+	stable_sort(pointVec.begin() , pointVec.end() , compare);
 	
 	for(const Point& p : pointVec)
 	{
