25039bdb83f1518c5419985dd76c83c0b39593f8
kimdanang7131 <91853084+kimdanang7131@users.noreply.github.com>
2024-08-20 18:41:13 +0900

    [Silver V] Title: 단어 정렬, Time: 16 ms, Memory: 3976 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.cc
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.cc
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <map>
 #include <algorithm>
 
 using namespace std;
@@ -11,11 +10,7 @@ bool cmp(const string& a , const string& b)
 {
 	if(a.length() == b.length())
 	{
-		for(int i=0; i<a.length(); i++)
-		{
-			if(a[i] != b[i])
-				return a[i] < b[i];
-		}
+		return a < b;
 	}
 	else
 		return a.length() < b.length();
@@ -24,32 +19,26 @@ bool cmp(const string& a , const string& b)
 int main()
 { 
 	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
 	cin >> n;
-	vector<string> wordVec;
-	map<string,bool> wordMap;
+	cin.ignore();
+	
+	vector<string> wordVec(n);
 	
 	for(int i=0; i<n; i++)
 	{
-		string tmp;
-		cin >> tmp;
-		
-		if(wordMap.count(tmp) > 0)
-			continue;
-		
-		wordMap[tmp] = true;
+		cin >> wordVec[i];
 	}
 	
-	for(const pair<string,bool>& p : wordMap)
-	{
-		wordVec.emplace_back(p.first);
-	}
 	
 	sort(wordVec.begin() , wordVec.end() , cmp);
-	
+	wordVec.erase(unique(wordVec.begin() , wordVec.end()), wordVec.end());
+		
 	for(const string& s : wordVec)
 	{
-		cout << s << endl;
+		cout << s << '\n';
 	}
 	
 	return 0;
