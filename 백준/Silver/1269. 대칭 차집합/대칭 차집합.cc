a0be762f6767d8b01187ff723cbdb1b939285f07
kimdanang7131 <91853084+kimdanang7131@users.noreply.github.com>
2024-09-11 18:22:49 +0900

    [Silver IV] Title: 대칭 차집합, Time: 132 ms, Memory: 21428 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.cc
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.cc
@@ -1,37 +1,28 @@
 #include <iostream>
-#include <vector>
+#include <unordered_set>
 
 using namespace std;
 
-int n , m;
+int n, m;
 
-int main()
-{ 
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	//cout.tie(NULL);
-	
-	
-	cin >> n >> m;
-	vector<bool> v(100000001);
-	
-	for(int i=0; i < n + m; i++)
-	{
-		int idx = 0;
-		cin >> idx;
-		
-		v[idx] = !v[idx];
-	}
-	
-	int ans = 0;
-	
-	for(int i=0; i<100000001; i++)
-	{
-		if(v[i] == 1)
-			ans++;
-	}
-	
-	cout << ans;
-	
-	return 0;
+int main() 
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> n >> m;
+    unordered_set<int> s;
+
+    for (int i = 0; i < n + m; i++) {
+        int x;
+        cin >> x;
+        if (s.find(x) != s.end()) 
+            s.erase(x);
+		else
+            s.insert(x);
+    }
+
+    cout << s.size();
+
+    return 0;
 }
