cdb6e906ccc4dfb51fc70f78492e14c9cf114056
kimdanang7131 <91853084+kimdanang7131@users.noreply.github.com>
2024-10-22 02:47:07 +0900

    [Bronze IV] Title: 녹색거탑, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int main() 
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+	int n;
+	cin >> n;
+	
+	cout << ( 1 << n ) << endl;
+	return 0;
+}
