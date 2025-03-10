0fa3355e668d0f682d2e661deb314019ddb2918c
kimdanang7131 <91853084+kimdanang7131@users.noreply.github.com>
2025-03-11 03:57:59 +0900

    [Silver II] Title: 최대 힙, Time: 12 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+#define endl '\n'
+#define INF 0x7ffffff
+
+int n;
+
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+	cin >> n;
+	
+	priority_queue<int> pq;
+	
+	while(n--)
+	{
+		int op;
+		cin >> op;
+		
+		if(op == 0)
+		{
+			if(pq.empty())
+			{
+				cout << 0 << endl;
+			}
+			else
+			{
+				cout << pq.top() << endl;
+				pq.pop();
+			}
+		}
+		else
+		{
+			pq.push(op);
+		}
+	}
+
+    return 0;
+}
